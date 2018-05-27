ac7efa40b5dcc28cb42544277941e44207e2f483
Takahiro Oshima <tarotora51@gmail.com>
2018-05-28 08:26:39 +0900

    LDAPの部分は削除

---
+++ countries/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+func main() {
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		rest.Get("/countries", GetAllCountries),
+		rest.Post("/countries", PostCountry),
+		rest.Get("/countries/:code", GetCountry),
+		rest.Delete("/countries/:code", DeleteCountry),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+}
+
+type Country struct {
+	Code string
+	Name string
+}
+
+var store = map[string]*Country{}
+
+var lock = sync.RWMutex{}
+
+func GetAllCountries(w rest.ResponseWriter, r *rest.Request) {
+	lock.RLock()
+	countries := make([]Country, len(store))
+	i := 0
+	for _, country := range store {
+		countries[i] = *country
+		i++
+	}
+	lock.RUnlock()
+	w.WriteJson(&countries)
+}
+
+func PostCountry(w rest.ResponseWriter, r *rest.Request) {
+	country := Country{}
+	err := r.DecodeJsonPayload(&country)
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if country.Code == "" {
+		rest.Error(w, "Country Code is Required!!", 400)
+	}
+	if country.Name == "" {
+		rest.Error(w, "Country Name is Required !!!", 400)
+	}
+	lock.Lock()
+	store[country.Code] = &country
+	lock.Unlock()
+	w.WriteJson(&country)
+}
+
+func GetCountry(w rest.ResponseWriter, r *rest.Request) {
+	code := r.PathParam("code")
+
+	lock.RLock()
+	var country *Country
+	if store[code] != nil {
+		country = &Country{}
+		*country = *store[code]
+	}
+	lock.RUnlock()
+
+	if country == nil {
+		rest.NotFound(w, r)
+		return
+	}
+	w.WriteJson(country)
+}
+
+func DeleteCountry(w rest.ResponseWriter, r *rest.Request) {
+	code := r.PathParam("code")
+	lock.Lock()
+	delete(store, code)
+	lock.Unlock()
+	w.WriteHeader(http.StatusOK)
+}
--- main.go
+++ main.go
@@ -1,95 +1,76 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
-	"sync"
 
-	"github.com/ant0ine/go-json-rest/rest"
-	ldap "gopkg.in/ldap.v2"
+	"github.com/gorilla/mux"
 )
 
+var people []Person
+
 func main() {
-	api := rest.NewApi()
-	api.Use(rest.DefaultDevStack...)
+	router := mux.NewRouter()
 
-	api.Use(&rest.CorsMiddleware{
-		RejectNonCorsRequests: false,
-		OriginValidator: func(origin string, request *rest.Request) bool {
-			return origin == "http://localhost:3001"
-		},
-		AllowedMethods: []string{"GET", "POST", "PUT"},
-		AllowedHeaders: []string{
-			"Accept", "Content-Type", "X-Custom-Header", "Origin"},
-		AccessControlAllowCredentials: true,
-		AccessControlMaxAge:           3600,
-	})
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
 
-	router, err := rest.MakeRouter(
-		rest.Get("/users", GetAllLdapUsers),
-	)
-	if err != nil {
-		log.Fatal(err)
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// GetPeople : Return Person list
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+
+// GetPerson : Return a Person by id
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+		}
 	}
-	api.SetApp(router)
-	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
 
-type Graph struct {
-	Nodes []*Node
-	Links []*Link
+// CreatePerson : Create Person by posted id
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 
-type Node struct {
-	id    string
-	group int
-}
-
-type Link struct {
-	Source string
-	Target string
-}
-
-var lock = sync.RWMutex{}
-
-// GetAllLdapUsers : get users in ldap
-func GetAllLdapUsers(w rest.ResponseWriter, r *rest.Request) {
-	lock.RLock()
-	userEntries := LdapUsersSearch()
-	//group Entries
-
-	// convert Entries to graph model json
-	//graphJSON := jsonConverter(userEntries)
-	lock.RUnlock()
-	w.WriteJson(&userEntries)
-}
-
-// LdapUsersSearch : return Entries of LDAP users
-func LdapUsersSearch() []*ldap.Entry {
-	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", "localhost", 389))
-	if err != nil {
-		log.Fatal(err)
+// DeletePerson :  Delete a Person by id
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
 	}
-	defer l.Close()
+}
 
-	err = l.Bind("cn=admin,dc=example,dc=org", "admin")
-	if err != nil {
-		log.Fatal(err)
-	}
+// Person object
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
 
-	searchRequest := ldap.NewSearchRequest(
-		"dc=example,dc=org", // The base dn to search
-		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		"(&(objectClass=inetOrgPerson))",    // The filter to apply
-		[]string{"dn", "cn", "displayName"}, // A list attributes to retrieve
-		nil,
-	)
-
-	sr, err := l.Search(searchRequest)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return sr.Entries
+// Address object : not has a id
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
 }
