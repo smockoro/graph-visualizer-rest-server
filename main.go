fadbde900ec3b4fbadeb270d79e2e85d45f72774
Takahiro Oshima <tarotora51@gmail.com>
2018-06-03 14:06:48 +0900

    mis成功

--- main.go
+++ main.go
@@ -2,75 +2,64 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 
-	"github.com/gorilla/mux"
+	"github.com/ant0ine/go-json-rest/rest"
 )
 
-var people []Person
-
 func main() {
-	router := mux.NewRouter()
-
-	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
-
-	router.HandleFunc("/people", GetPeople).Methods("GET")
-	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-// GetPeople : Return Person list
-func GetPeople(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(people)
-}
-
-// GetPerson : Return a Person by id
-func GetPerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, item := range people {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-		}
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	api.Use(&rest.CorsMiddleware{
+		RejectNonCorsRequests: false,
+		OriginValidator: func(origin string, request *rest.Request) bool {
+			return origin == "http://localhost:3001"
+		},
+		AllowedMethods: []string{"GET", "POST", "PUT"},
+		AllowedHeaders: []string{
+			"Accept", "Content-Type", "X-Custom-Header", "Origin"},
+		AccessControlAllowCredentials: true,
+		AccessControlMaxAge:           3600,
+	})
+	router, err := rest.MakeRouter(
+		rest.Get("/graph", GetGraphMis),
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
+	api.SetApp(router)
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
 
-// CreatePerson : Create Person by posted id
-func CreatePerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var person Person
-	_ = json.NewDecoder(r.Body).Decode(&person)
-	person.ID = params["id"]
-	people = append(people, person)
-	json.NewEncoder(w).Encode(people)
+type Graph struct {
+	Nodes []struct {
+		ID    int    `json:"id"`
+		Label string `json:"label"`
+		Group int    `json:"group"`
+	} `json:"nodes"`
+	Edges []struct {
+		From int `json:"from"`
+		To   int `json:"to"`
+	} `json:"edges"`
 }
 
-// DeletePerson :  Delete a Person by id
-func DeletePerson(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for index, item := range people {
-		if item.ID == params["id"] {
-			people = append(people[:index], people[index+1:]...)
-			break
-		}
-		json.NewEncoder(w).Encode(people)
+var lock = sync.RWMutex{}
+
+func GetGraphMis(w rest.ResponseWriter, r *rest.Request) {
+	lock.RLock()
+	bytes, err := ioutil.ReadFile("./graph_mis.json")
+	if err != nil {
+		log.Fatal(err)
 	}
-}
 
-// Person object
-type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
-}
+	var graph Graph
+	if err := json.Unmarshal(bytes, &graph); err != nil {
+		log.Fatal(err)
+	}
+	lock.RUnlock()
 
-// Address object : not has a id
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
+	w.WriteJson(graph)
 }
